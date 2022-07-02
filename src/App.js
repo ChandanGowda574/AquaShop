1a1c370762b99b1c72774658bd22dde7c7afc12b
Chandan Gowda <chandangowda574@gmail.com>
2022-07-02 18:53:38 +0530

    Added react-router-dom and basic routing

--- src/App.js
+++ src/App.js
@@ -1,36 +1,24 @@
-import Directory from './components/directory/directory.components';
-import './App.css'
+import { Routes, Route } from "react-router-dom";
+import Home from "./routes/home/home.component";
+import Navigation from "./routes/navigation/navigation.comonent";
+import "./App.css";
 
-const App =()=> {
-  const categories = [
-    {
-      id: 1,
-      title: 'Fishes',
-      imageUrl: 'https://wallpaperaccess.com/full/747278.jpg',
-    },
-    {
-      id: 2,
-      title: 'Supplies',
-      imageUrl: 'https://www.aquariumcarebasics.com/images/power-filter-impeller-assembly.webp',
-    },
-    {
-      id: 3,
-      title: 'Foods',
-      imageUrl: 'https://img2.exportersindia.com/product_images/bc-full/2019/7/4087215/fish-food-1562310580-4984245.jpeg',
-    },
-    {
-      id: 4,
-      title: 'Plants',
-      imageUrl: 'https://www.aquariumcarebasics.com/images/ludwigia-peruensis-plant.webp',
-    },
-    {
-      id: 5,
-      title: 'Aquarium',
-      imageUrl: 'https://0201.nccdn.net/1_2/000/000/09e/0e9/Wallace-640x853.jpg#RDAMDAID13737549',
-    },
-  ];
- 
-    return <Directory categories={categories} />;
+const Shop = () =>{
+  return (
+    <div className="shop">
+    <h1>I am Shop component</h1>
+     </div>
+  );
 }
+const App = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<Navigation />} >
+      <Route path="/" index element={<Home />} />
+      <Route path="shop" element={<Shop />} />
+      </Route>
+    </Routes>
+  );
+};
 
 export default App;
---
+++ src/routes/home/home.component.jsx
@@ -0,0 +1,42 @@
+import Directory from "../../components/directory/directory.components";
+import { Outlet } from "react-router-dom";
+
+const Home =()=> {
+    const categories = [
+      {
+        id: 1,
+        title: 'Fishes',
+        imageUrl: 'https://wallpaperaccess.com/full/747278.jpg',
+      },
+      {
+        id: 2,
+        title: 'Supplies',
+        imageUrl: 'https://www.aquariumcarebasics.com/images/power-filter-impeller-assembly.webp',
+      },
+      {
+        id: 3,
+        title: 'Foods',
+        imageUrl: 'https://img2.exportersindia.com/product_images/bc-full/2019/7/4087215/fish-food-1562310580-4984245.jpeg',
+      },
+      {
+        id: 4,
+        title: 'Plants',
+        imageUrl: 'https://www.aquariumcarebasics.com/images/ludwigia-peruensis-plant.webp',
+      },
+      {
+        id: 5,
+        title: 'Aquarium',
+        imageUrl: 'https://0201.nccdn.net/1_2/000/000/09e/0e9/Wallace-640x853.jpg#RDAMDAID13737549',
+      },
+    ];
+   
+      return (
+        <div>
+          <Directory categories={categories} />
+          <Outlet />
+        </div>
+      );
+  }
+  
+  export default  Home;
+  

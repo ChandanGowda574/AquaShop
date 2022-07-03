d6af6b7741e8eaeaad689d351d4a9a4bf0ba5aa7
Chandan Gowda <chandangowda574@gmail.com>
2022-07-03 12:30:02 +0530

    Added the product-card and productprovider

--- src/App.js
+++ src/App.js
@@ -3,20 +3,15 @@ import Home from "./routes/home/home.component";
 import Navigation from "./routes/navigation/navigation.comonent";
 import Authentication from "./routes/authentication/authentication.component";
 import "./App.css";
+import Shop from "./routes/shop/shop.component";
 
-const Shop = () =>{
-  return (
-    <div className="shop">
-    <h1>I am Shop component</h1>
-     </div>
-  );
-}
+
 const App = () => {
   return (
     <Routes>
       <Route path="/" element={<Navigation />} >
       <Route path="/" index element={<Home />} />
-      <Route path="shop" element={<Shop />} />
+      <Route path="shop" element={<Shop/>} />
       <Route path='auth' element={<Authentication />} />
       </Route>
     </Routes>
